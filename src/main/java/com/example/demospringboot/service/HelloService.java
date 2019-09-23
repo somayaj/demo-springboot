4691740eb9152cf0ce3692e77135b15ff1e32b60
Asha Somayajula <somayajulaas@gmail.com>
2019-09-23 16:32:17 -0500

    Update HelloService.java

--- src/main/java/com/example/demospringboot/service/HelloService.java
+++ src/main/java/com/example/demospringboot/service/HelloService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class HelloService {
     public String getHello()
     {
-        return "<html><head><script type="text/javascript" src="/ruxitagentjs_23SVfqrtu_10175190917092722.js" data-dtconfig="app=0ad6efa3f923817b|featureHash=23SVfqrtu|srsr=25000|reportUrl=/rb_bf98707dez|rdnt=1|uxrgce=1|bp=2|srms=1,1,,,|uxrgcm=100,25,300,3;100,25,300,3|dpvc=1|lastModification=1569272835408|dtVersion=10175190917092722|tp=500,50,0,1|uxdcw=1500|tal=3"></script></head><body>hello world</body></html>";
+        return "<html><head><script type=\"text/javascript\" src=\"/ruxitagentjs_23SVfqrtu_10175190917092722.js\" data-dtconfig=\"app=0ad6efa3f923817b|featureHash=23SVfqrtu|srsr=25000|reportUrl=/rb_bf98707dez|rdnt=1|uxrgce=1|bp=2|srms=1,1,,,|uxrgcm=100,25,300,3;100,25,300,3|dpvc=1|lastModification=1569272835408|dtVersion=10175190917092722|tp=500,50,0,1|uxdcw=1500|tal=3\"></script></head><body>hello world</body></html>";
     }
 }
